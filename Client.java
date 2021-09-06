499dc281066bac9c8fc707feb05579ac62eb4523
Mohamed Sammari <mohammed.sammari@telecom-sudparis.eu>
2021-09-06 21:47:07 +0200

    Client app

---
+++ Client.java
@@ -0,0 +1,36 @@
+package NetworkJavaApp;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+	public Client() throws Exception {
+		
+		Socket socket = new Socket("localhost",3030);
+		System.out.println("Successful connection to the server.");
+		
+		// I/O streams
+		BufferedReader in_socket = new BufferedReader (new InputStreamReader (socket.getInputStream()));
+		PrintWriter out_socket = new PrintWriter (new OutputStreamWriter (socket.getOutputStream()), true);
+		
+		String message = in_socket.readLine();
+		System.out.println("Server says: " + message);
+		out_socket.println("Thanks!");
+		
+		socket.close();
+		System.out.println("Socket closed.");
+		
+	}
+	
+	public static void main(String[] args) {
+		try {
+			new Client();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
--- Server.java
+++ Server.java
@@ -1,3 +1,4 @@
+package NetworkJavaApp;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
